a0b36499d3d5289ffdc93c2da7747565a99d5994
invi <roflankaa@gmail.com>
2024-12-28 18:58:09 +0100

    update

--- src/algomap/io/DynamicProgramming.java
+++ src/algomap/io/DynamicProgramming.java
@@ -1,16 +1,6 @@
 package algomap.io;
 
 public class DynamicProgramming {
-    private Integer counter = 0;
-
-    public void inc() {
-        counter++;
-    }
-
-    public Integer run() {
-        return counter;
-    }
-
     public static void main(String[] args) {
     }
 }
